4e3d1b9ab84c41e4f12fed023a22b62e74c89f67
Tomas Baublys <tomas.baublys@mif.stud.vu.lt>
2025-11-23 13:42:53 +0200

    initial commit

---
+++ main/main.ino
@@ -0,0 +1,316 @@
+#include <SPI.h>
+#include <mcp2515.h>
+#include "pids.h"
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7789.h>
+
+// MCP2515
+MCP2515 mcp2515(10);
+struct can_frame canMsg;
+
+// TFT pins
+#define TFT_CS 7
+#define TFT_DC 6
+#define TFT_RST 8
+#define TFT_UPDATE_INT 50 // ms
+
+// Mode switching button
+#define MODE_BTN 3
+#define MODE_BTN_DEBOUNCE_TIME_MS 200
+
+#define MODES_SIZE 6
+
+enum Modes : byte {
+    DISP_BOOST = 0x00,
+    DISP_COOLANT = 0x01,
+    DISP_RPM = 0x02,
+    DISP_ELOAD = 0x03,
+    DISP_LAMBDA = 0x04,
+    DISP_FUEL_PRESSURE = 0x05
+};
+
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+// Values
+int rpm_val = 0;
+int coolant_temp_val = 0;
+int map_val = -1;
+int bar_val = -1;
+float boost_psi = 0;
+int fuel_pressure = 0;
+bool request_map = true;
+
+float engine_load_val = 0;
+float fuel_air_eqr_val = 0;
+
+// for pressing the button
+volatile bool button_pressed = false;
+unsigned long last_pressed_time = 0;
+
+// current mode of what to send and what to display
+byte curr_mode = DISP_BOOST;
+
+// old values for smooth display
+int old_rpm_val = -9999;
+int old_coolant_val = -9999;
+float old_boost_psi = -9999;
+float old_lambda_val = -9999;
+float old_eload_val = -9999;
+int old_fuel_pressure = -9999;
+
+void send_PID(byte mode, byte pid) {
+    canMsg.can_id  = 0x7DF;
+    canMsg.can_dlc = 8;
+    canMsg.data[0] = 0x02;
+    canMsg.data[1] = mode;
+    canMsg.data[2] = pid;
+
+    for (int i = 3; i < 8; i++)
+        canMsg.data[i] = 0;
+
+    mcp2515.sendMessage(&canMsg);
+}
+
+void draw_unit(const char* unit) {
+    tft.setTextSize(3);
+    tft.setCursor(170, 10); 
+    tft.print(unit);
+}
+
+void draw_mode_screen() {
+    tft.fillScreen(ST77XX_BLACK); 
+    
+    tft.setCursor(10, 10);
+    tft.setTextSize(3);
+
+    // reset old values to force a redraw
+    old_rpm_val = -9999; 
+    old_coolant_val = -9999;
+    old_boost_psi = -9999;
+    old_lambda_val = -9999;
+    old_eload_val = -9999;
+    old_fuel_pressure = -9999;
+
+    switch (curr_mode) {
+        case DISP_RPM:
+            tft.println("RPM");
+            break;
+        case DISP_COOLANT:
+            tft.println("Coolant");
+            draw_unit("C");
+            break;
+        case DISP_BOOST:
+            tft.println("Boost");
+            draw_unit("psi");
+            break;
+        case DISP_LAMBDA:
+            tft.println("Lambda");
+            draw_unit("EQ");
+            break; 
+        case DISP_ELOAD:
+            tft.println("E. Load");
+            draw_unit("%");
+            break;
+        case DISP_FUEL_PRESSURE: 
+            tft.println("Fuel Press");
+            draw_unit("kPa");
+        default:
+            tft.println("ERROR");
+            break;
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+    SPI.begin();
+
+    // TFT
+    tft.init(240, 240);
+    tft.setRotation(3);
+    tft.fillScreen(ST77XX_BLACK);
+    
+    tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
+    
+    tft.setTextWrap(false);
+
+    tft.setTextSize(5);
+    tft.setCursor(20, 40);
+    tft.println("Booting...");
+
+    // MCP2515
+    mcp2515.reset();
+    mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);
+    mcp2515.setNormalMode();
+    
+    // Mode button
+    pinMode(MODE_BTN, INPUT_PULLUP);
+
+    // interrupt for the button
+    attachInterrupt(digitalPinToInterrupt(MODE_BTN), on_button_press, FALLING);
+
+    delay(200);
+    draw_mode_screen();
+}
+
+void loop() {
+    // get the current time
+    unsigned long time_now = millis();
+
+    static unsigned long last_request = 0;
+
+    if(button_pressed) {
+        handle_pressed_button();
+    }
+
+    // send the request based which mode we are in
+    if(time_now - last_request > SAFE_PID_TIMEOUT) {
+        switch(curr_mode) {
+            case Modes::DISP_RPM:
+                send_PID(CURRENT_DATA_MODE, ENGINE_RPM);
+                break;
+            case Modes::DISP_LAMBDA:
+                send_PID(CURRENT_DATA_MODE, FUEL_AIR_EQUIV_RATIO);
+                break;
+            case Modes::DISP_ELOAD:
+                send_PID(CURRENT_DATA_MODE, CALCULATED_ENGINE_LOAD);
+                break;
+            case Modes::DISP_COOLANT:
+                send_PID(CURRENT_DATA_MODE, COOLANT_TEMP);
+                break;
+            case Modes::DISP_BOOST:
+                if(request_map) {
+                    send_PID(CURRENT_DATA_MODE, INTAKE_MANIFOLD_PRESSURE);
+                }
+                else {
+                    send_PID(CURRENT_DATA_MODE, BAROMETRIC_PRESSURE);
+                }
+                request_map = !request_map;
+                break;
+            case Modes::DISP_FUEL_PRESSURE:
+                send_PID(CURRENT_DATA_MODE, FUEL_PRESSURE);
+                break;
+            default:
+                break;
+        }
+        last_request = time_now;
+    }
+
+
+    // Read incoming CAN frames
+    while (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
+        if (canMsg.can_id >= 0x7E8 && canMsg.can_id <= 0x7EF &&
+            canMsg.data[1] == CURRENT_DATA_RESP) {
+
+            byte pid = canMsg.data[2];
+            byte a = canMsg.data[3];
+            byte b = canMsg.data[4];
+
+            switch (pid) {
+                case ENGINE_RPM:
+                    rpm_val = pid_calc::rpm(a, b);
+                    break;
+                case COOLANT_TEMP:
+                    coolant_temp_val = pid_calc::coolant_temp(a);
+                    break;
+                case INTAKE_MANIFOLD_PRESSURE:
+                    map_val = pid_calc::map_kpa(a);
+                    boost_psi = pid_calc::boost_psi(map_val, bar_val);
+                    break;
+                case BAROMETRIC_PRESSURE:
+                    bar_val = pid_calc::bap_kpa(a);
+                    boost_psi = pid_calc::boost_psi(map_val, bar_val);
+                    break;
+                case CALCULATED_ENGINE_LOAD:
+                    engine_load_val = pid_calc::engine_load(a);
+                    break;
+                case FUEL_AIR_EQUIV_RATIO:
+                    fuel_air_eqr_val = pid_calc::fuel_air_eq_ration(a, b);
+                    break;
+                case FUEL_PRESSURE:
+                    fuel_pressure = pid_calc::fuel_pressure(a);
+                    break;
+                default: 
+                    break;
+            }
+        }
+    }
+    
+    static unsigned long last_screen_update = 0;
+    if (millis() - last_screen_update > TFT_UPDATE_INT) { 
+        switch (curr_mode) {
+            case DISP_RPM:
+                if (rpm_val != old_rpm_val) {
+                    tft.setCursor(10, 100);
+                    tft.setTextSize(6);
+                    tft.print(rpm_val);
+                    tft.print("   "); // Padding
+                    old_rpm_val = rpm_val;
+                }
+                break;
+            case DISP_COOLANT:
+                if (coolant_temp_val != old_coolant_val) {
+                    tft.setCursor(10, 100);
+                    tft.setTextSize(6);
+                    tft.print(coolant_temp_val);
+                    tft.print("   "); // Padding
+                    old_coolant_val = coolant_temp_val;
+                }
+                break;
+            case DISP_BOOST:
+                if (abs(boost_psi - old_boost_psi) > 0.01) {
+                    tft.setCursor(10, 100);
+                    tft.setTextSize(6);
+                    tft.print(boost_psi, 1);
+                    tft.print("   "); // Padding
+                    old_boost_psi = boost_psi;
+                }
+                break;
+            case DISP_LAMBDA:
+                if (abs(fuel_air_eqr_val - old_lambda_val) > 0.01) {
+                    tft.setCursor(10, 100);
+                    tft.setTextSize(6);
+                    tft.print(fuel_air_eqr_val, 2);
+                    tft.print("   "); // Padding
+                    old_lambda_val = fuel_air_eqr_val;
+                }
+                break;
+            case DISP_ELOAD:
+                if (abs(engine_load_val - old_eload_val) > 0.1) {
+                    tft.setCursor(10, 100);
+                    tft.setTextSize(6);
+                    tft.print(engine_load_val, 1);
+                    tft.print("   "); // Padding
+                    old_eload_val = engine_load_val;
+                }
+                break;
+            case DISP_FUEL_PRESSURE:
+                if(fuel_pressure != old_fuel_pressure) {
+                    tft.setCursor(10, 100);
+                    tft.setTextSize(6);
+                    tft.print(fuel_pressure, 1);
+                    tft.print("   "); // Padding
+                    old_fuel_pressure = fuel_pressure;
+                }
+            default:
+                break;
+        }
+
+        last_screen_update = millis();
+    }
+}
+
+void on_button_press() {
+    unsigned long time_now = millis();
+    if(time_now - last_pressed_time > MODE_BTN_DEBOUNCE_TIME_MS) {
+        button_pressed = true;
+        last_pressed_time = time_now;
+    }
+}
+
+void handle_pressed_button() {
+    curr_mode = (curr_mode + 1) % MODES_SIZE;
+    button_pressed = false;
+    draw_mode_screen();
+}
---
+++ main/pids.h
@@ -0,0 +1,128 @@
+#ifndef PIDS_H_INCLUDED
+#define PIDS_H_INCLUDED
+
+// OBD2 PIDS
+// MODE 01 – STANDARD OBD-II PIDs (SAE J1979)
+#define SAFE_PID_TIMEOUT 100
+
+#define CURRENT_DATA_MODE         0x01
+#define CURRENT_DATA_RESP         0x41
+
+// 0x00–0x1F
+#define SUPPORTED_PIDS_00_20      0x00
+#define MONITOR_STATUS_SINCE_DTC  0x01
+#define FREEZE_DTC                0x02
+#define FUEL_SYSTEM_STATUS        0x03
+#define CALCULATED_ENGINE_LOAD    0x04
+#define COOLANT_TEMP              0x05
+#define STFT_BANK1                0x06
+#define LTFT_BANK1                0x07
+#define STFT_BANK2                0x08
+#define LTFT_BANK2                0x09
+#define FUEL_PRESSURE             0x0A
+#define INTAKE_MANIFOLD_PRESSURE  0x0B
+#define ENGINE_RPM                0x0C
+#define VEHICLE_SPEED             0x0D
+#define TIMING_ADVANCE            0x0E
+#define INTAKE_AIR_TEMP           0x0F
+#define MAF_AIRFLOW_RATE          0x10
+#define THROTTLE_POSITION         0x11
+#define SECONDARY_AIR_STATUS      0x12
+#define O2_SENSORS_PRESENT        0x13
+#define O2_SENSOR1_VOLTAGE_STFT   0x14
+#define O2_SENSOR2_VOLTAGE_STFT   0x15
+#define O2_SENSOR3_VOLTAGE_STFT   0x16
+#define O2_SENSOR4_VOLTAGE_STFT   0x17
+#define O2_SENSOR5_VOLTAGE_STFT   0x18
+#define O2_SENSOR6_VOLTAGE_STFT   0x19
+#define O2_SENSOR7_VOLTAGE_STFT   0x1A
+#define O2_SENSOR8_VOLTAGE_STFT   0x1B
+#define OBD_STANDARDS             0x1C
+#define O2_SENSORS_PRESENT_ALT    0x1D
+#define AUX_INPUT_STATUS          0x1E
+#define RUN_TIME_WITH_ENGINE_ON   0x1F
+
+// 0x20–0x3F
+#define SUPPORTED_PIDS_20_40      0x20
+#define DIST_SINCE_DTC_CLEAR      0x21
+#define EVAPORATIVE_PURGE         0x22
+#define FUEL_RAIL_PRESSURE        0x23
+#define FUEL_RAIL_GAUGE_PRESSURE  0x24
+#define O2_S1_WR_CURRENT          0x24
+#define O2_S2_WR_CURRENT          0x25
+#define O2_S3_WR_CURRENT          0x26
+#define O2_S4_WR_CURRENT          0x27
+#define O2_S5_WR_CURRENT          0x28
+#define O2_S6_WR_CURRENT          0x29
+#define O2_S7_WR_CURRENT          0x2A
+#define O2_S8_WR_CURRENT          0x2B
+#define COMMANDED_EGR             0x2C
+#define EGR_ERROR                 0x2D
+#define COMMANDED_EVAP_PURGE      0x2E
+#define FUEL_TANK_LEVEL_INPUT     0x2F
+#define WARM_UPS_SINCE_DTC_CLEAR  0x30
+#define DISTANCE_SINCE_DTC_CLEAR  0x31
+#define EVAP_SYSTEM_VAPOR_PRESS   0x32
+#define BAROMETRIC_PRESSURE       0x33
+
+// 0x3C–0x5F
+#define SUPPORTED_PIDS_40_60      0x40
+#define CONTROL_MODULE_VOLTAGE    0x42
+#define ENGINE_LOAD_ABSOLUTE      0x43
+#define FUEL_AIR_EQUIV_RATIO      0x44
+#define RELATIVE_THROTTLE         0x45
+#define AMBIENT_AIR_TEMP          0x46
+#define ABSOLUTE_THROTTLE_POSB    0x47
+#define ABSOLUTE_THROTTLE_POSC    0x48
+#define ACCELERATOR_PEDAL_POSD    0x49
+#define ACCELERATOR_PEDAL_POSE    0x4A
+#define ACCELERATOR_PEDAL_POSF    0x4B
+#define THROTTLE_ACTUATOR         0x4C
+#define RUN_TIME_MIL_ON           0x4D
+#define TIME_SINCE_DTC_CLEARED    0x4E
+
+#define ENGINE_OIL_TEMP           0x5C
+
+namespace pid_calc {
+  inline int rpm(int a, int b) {
+    return (256 * a + b) / 4;
+  }
+  
+  inline int coolant_temp(int a) {
+    return a - 40;
+  }
+
+  inline int map_kpa(int a) {
+    return a;
+  }
+
+  inline int bap_kpa(int a) {
+    return a;
+  }
+
+  inline float boost_bar(int map_kpa, int bap_kpa) {
+    return (map_kpa - bap_kpa) / 100.0f;
+  }
+
+  inline float boost_psi(int map_kpa, int bap_kpa) {
+    return (map_kpa - bap_kpa) * 0.1450377f;
+  }
+
+  inline float engine_load(int a) {
+    return (a * 100.0f) / 255.0f;  
+  }
+
+  inline int speed(int a) {
+    return a;
+  }
+
+  inline float fuel_air_eq_ration(int a, int b) {
+    return (2.0f / 65536.0f) * (256 * a + b);
+  }
+
+  inline int fuel_pressure(int a) {
+    return 3 * a;
+  }
+}
+
+#endif // PIDS_H_INCLUDED
